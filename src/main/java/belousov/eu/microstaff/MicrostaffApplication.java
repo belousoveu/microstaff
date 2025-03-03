9d225e2637496920d979ea1f17d0d9a3ae52125d
Belousoveu <belousov.eu@gmail.com>
2025-03-04 02:53:39 +0300

    Initial commit

---
+++ src/main/java/belousov/eu/microstaff/MicrostaffApplication.java
@@ -0,0 +1,13 @@
+package belousov.eu.microstaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrostaffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/belousov/eu/microstaff/MicrostaffApplicationTests.java
@@ -0,0 +1,13 @@
+package belousov.eu.microstaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostaffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
