dff1eee28a4a40ac8498b02c7efe58e6d83e96b2
Belousoveu <belousov.eu@gmail.com>
2025-03-04 15:39:41 +0300

    docs: OpenAPI/Swagger has been integrated and configured within the project. Annotations have been set up to generate OpenAPI documentation.

--- company-service/src/main/java/belousov/eu/companyservice/aspect/CompanyControllerAdvice.java
+++ company-service/src/main/java/belousov/eu/companyservice/aspect/CompanyControllerAdvice.java
@@ -2,6 +2,7 @@ package belousov.eu.companyservice.aspect;
 
 
 import belousov.eu.companyservice.exception.CompanyNotFoundException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,5 +14,10 @@ public class CompanyControllerAdvice {
     @ExceptionHandler(CompanyNotFoundException.class)
     public ResponseEntity<String> handleCompanyNotFoundException(CompanyNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(DataAccessException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ex.getMessage());
     }
 }
